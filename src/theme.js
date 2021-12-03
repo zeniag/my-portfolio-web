e31def24aeedfb8d315d7467380eb0029ea0123d
Zenia Gist <zg@buildmidwestern.com>
2021-12-03 14:46:10 -0600

    add global style theme

--- src/theme.js
+++ src/theme.js
@@ -15,9 +15,36 @@ const theme = createTheme({
     },
     mode: "dark",
   },
-  typography: {},
-  // overrides
-  components: {},
+  typography: {
+    fontFamily: ["Barlow", "Oswald", "Anton", "san-serif"].join(","),
+    h1: { fontWeight: 500, fontSize: 60 },
+    h2: { fontWeight: 500, fontSize: 60 },
+    h3: { fontWeight: 500 },
+    h4: { fontWeight: 500 },
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundColor: grey["A200"],
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        contained: {
+          backgroundColor: "#ff3f3f",
+        },
+      },
+    },
+    MuiToolbar: {
+      styleOverrides: {
+        regular: {
+          minHeight: 64,
+        },
+      },
+    },
+  },
 })
 
 export default theme
