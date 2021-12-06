5c3ee1faf3933ac3b114ec1ac692bb0f0440afee
Zenia Gist <zg@buildmidwestern.com>
2021-12-06 12:03:23 -0600

    added footer content and styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,50 @@
+import { Avatar, Paper } from "@mui/material"
 import React from "react"
 
+import * as footerStyles from "./Footer.module.scss"
+import gatsby from "../../images/avatars/gatsby.svg"
+import graphql from "../../images/avatars/graphql.svg"
+import sanity from "../../images/avatars/sanity.svg"
+import materialUI from "../../images/avatars/materialUI.svg"
+import react from "../../images/avatars/react.svg"
+
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <div className={footerStyles.root}>
+      <Paper className={footerStyles.footer} square>
+        <Avatar
+          variant="square"
+          alt="Gatsby"
+          src={gatsby}
+          className={footerStyles.small}
+        />
+        <Avatar
+          variant="square"
+          alt="GraphQL"
+          src={graphql}
+          className={footerStyles.small}
+        />
+        <Avatar
+          variant="square"
+          alt="Sanity"
+          src={sanity}
+          className={footerStyles.small}
+        />
+        <Avatar
+          variant="square"
+          alt="Material UI"
+          src={materialUI}
+          className={footerStyles.small}
+        />
+        <Avatar
+          variant="square"
+          alt="React"
+          src={react}
+          className={footerStyles.small}
+        />
+      </Paper>
+    </div>
+  )
 }
 
 export default Footer
