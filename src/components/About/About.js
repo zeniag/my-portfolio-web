66ec8c61c3700912a26169d47d19c3760a9e2c8f
Zenia Gist <zg@buildmidwestern.com>
2021-12-07 08:36:35 -0600

    updated styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import { Avatar, Container, Grid, Paper, Typography } from "@mui/material"
 import React from "react"
 
 import * as aboutStyles from "./About.module.scss"
-import SkillSet from "../Skillset/SkillSet"
+import SkillSet from "../SkillSet/SkillSet"
 import Bio from "../Bio/Bio"
 
 const About = () => {
--- src/components/SkillSet/SkillSet.js
+++ src/components/SkillSet/SkillSet.js
@@ -9,7 +9,7 @@ const SkillSet = () => {
       <Typography variant="h3" className={skillSetStyles.sectionHeading}>
         Skill Set
       </Typography>
-      <Typography variant="h5">
+      <Typography variant="body1">
         Here is a quick list of the languages and tools I have acquired over the
         years. Most of these I am currently using in recent projects.
       </Typography>
